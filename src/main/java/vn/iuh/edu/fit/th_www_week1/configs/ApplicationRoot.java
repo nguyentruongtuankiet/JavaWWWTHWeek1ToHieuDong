387f27836a6bbcba0fc69a19f22215cae4afcd01
TuanKiet <nguyentruongtuankiet2002@gmail.com>
2023-09-20 19:36:41 +0700

    create project

---
+++ src/main/java/vn/iuh/edu/fit/th_www_week1/configs/ApplicationRoot.java
@@ -0,0 +1,9 @@
+package vn.iuh.edu.fit.th_www_week1.configs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationRoot extends Application {
+
+}
